829b45891789e7ef37159f1efbdbffe0f97abc31
ALVI A V <149242620+A56-A5@users.noreply.github.com>
2025-05-09 22:10:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,50 @@
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require("path");
+
+const STROKES_FILE = path.join(__dirname, "strokes.json");
 
 app.use(express.static("public"));
 
+// Load strokes from file at startup
 let strokes = [];
+try {
+  if (fs.existsSync(STROKES_FILE)) {
+    strokes = JSON.parse(fs.readFileSync(STROKES_FILE, "utf8"));
+  }
+} catch (err) {
+  console.error("Failed to load strokes:", err);
+}
+
+function saveStrokes() {
+  fs.writeFileSync(STROKES_FILE, JSON.stringify(strokes, null, 2));
+}
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  // Send current strokes to the new user
   socket.emit("init", strokes);
 
   socket.on("stroke", (stroke) => {
     strokes.push(stroke);
+    saveStrokes();
     io.emit("stroke", stroke);
   });
 
   socket.on("undo", () => {
     if (strokes.length > 0) {
       strokes.pop();
-      io.emit("init", strokes); // Reset canvas for all
+      saveStrokes();
+      io.emit("init", strokes);
     }
   });
 
   socket.on("redo", (stroke) => {
     strokes.push(stroke);
+    saveStrokes();
     io.emit("stroke", stroke);
   });
 });
